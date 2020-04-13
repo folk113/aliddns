3f92447cfe50008156206f61b415209c4e9d1cc3
geyongliang <geyongliang@baidu.com>
2020-04-13 23:18:08 +0800

    直接使用强转来处理char转string的问题

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/folk113/aliddns/config"
 	"github.com/folk113/aliddns/dns"
 	"os"
@@ -9,8 +8,7 @@ import (
 )
 
 func main() {
-	separator := fmt.Sprintf("%c", os.PathSeparator)
-	index := strings.LastIndex(os.Args[0], separator)
+	index := strings.LastIndex(os.Args[0], string(os.PathSeparator))
 	path := os.Args[0][:index]
 	config.Init(path)
 	ip, err := dns.GetIp()
